4623514d4c40780967a85c250cb05acd1d16ca00
Prajaktahz <phzaavde@gmail.com>
2021-03-22 17:19:34 +0530

    First Commit

---
+++ SampleGitDemo/src/Calculator.java
@@ -0,0 +1,11 @@
+//https://github.com/Prajaktahz/myprojectrepo.git
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+
+	}
+
+}
